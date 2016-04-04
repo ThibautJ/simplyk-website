c3b9ee5ec376623c29644e9e944622e225208a15
ThibautJ <thibaut.jaurou@gmail.com>
2016-04-03 21:10:31 -0400

    Favicon and district 3 update

--- index.php
+++ index.php
@@ -32,26 +32,25 @@
   <!--<link rel="stylesheet" href="/octicons/octicons.css"> -->
 
   <!-- favicon -->
-  <link rel="shortcut icon" type="image/x-icon" href="favicon/favicon.ico">
-  <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="favicon/android-chrome-192x192.png" sizes="192x192">
-  <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96">
-  <link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="favicon/manifest.json">
-  <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="apple-mobile-web-app-title" content="Simplyk">
-  <meta name="application-name" content="Simplyk">
+  <link rel="apple-touch-icon" sizes="57x57" href="C:/wamp/www/tests/favicon/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="C:/wamp/www/tests/favicon/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="C:/wamp/www/tests/favicon/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="C:/wamp/www/tests/favicon/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="C:/wamp/www/tests/favicon/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="C:/wamp/www/tests/favicon/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="C:/wamp/www/tests/favicon/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="C:/wamp/www/tests/favicon/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="C:/wamp/www/tests/favicon/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/android-chrome-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="C:/wamp/www/tests/favicon/manifest.json">
+  <link rel="mask-icon" href="C:/wamp/www/tests/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="C:/wamp/www/tests/favicon/favicon.ico">
   <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+  <meta name="msapplication-TileImage" content="C:/wamp/www/tests/favicon/mstile-144x144.png">
+  <meta name="msapplication-config" content="C:/wamp/www/tests/favicon/browserconfig.xml">
   <meta name="theme-color" content="#ffffff">
 
 </head>
@@ -124,7 +123,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-4 center-block img-partners"><a href="http://d3center.ca/"><img src="http://d3center.ca/wp-content/uploads/2015/06/District3-Innovation-Center.png" class="img-responsive center-block"></a></div>
+      <div class="col-md-4 center-block img-partners"><a href="http://d3center.ca/"><img src="http://d3center.ca/wp-content/uploads/2016/04/weblogo02-01.png" class="img-responsive center-block"></a></div>
       <div class="col-md-4 center-block img-partners"><a href="http://cabm.net/en"><img src="https://pbs.twimg.com/profile_images/593854999202955264/xvNlID33.jpg" class="img-responsive center-block"></a></div>
       <div class="col-md-4 center-block img-partners"><a href="http://volunteerwica.com/accueuil/"><img src="http://volunteerwica.com/wp-content/themes/bhinneka-child/img/logofr.png" class="img-responsive center-block"></a></div>
       <!--<div class="col-md-3 center-block img-partners"><a href="https://www.concordia.ca/"><img src="http://5days.ca/concordia/wp-content/uploads/sites/25/2014/03/Concordia_Logo2.png" class="img-responsive center-block"></a></div>-->
--- team.php
+++ team.php
@@ -19,26 +19,25 @@
 	<!--<link rel="stylesheet" href="/octicons/octicons.css">-->
 
 	<!-- favicon -->
-	<link rel="shortcut icon" type="image/x-icon" href="favicon/favicon.ico">
-	<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="favicon/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="favicon/manifest.json">
-	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
-	<meta name="apple-mobile-web-app-title" content="Simplyk">
-	<meta name="application-name" content="Simplyk">
+	<link rel="apple-touch-icon" sizes="57x57" href="C:/wamp/www/tests/favicon/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="C:/wamp/www/tests/favicon/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="C:/wamp/www/tests/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="C:/wamp/www/tests/favicon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="C:/wamp/www/tests/favicon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="C:/wamp/www/tests/favicon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="C:/wamp/www/tests/favicon/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="C:/wamp/www/tests/favicon/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="C:/wamp/www/tests/favicon/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/android-chrome-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="C:/wamp/www/tests/favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="C:/wamp/www/tests/favicon/manifest.json">
+	<link rel="mask-icon" href="C:/wamp/www/tests/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="C:/wamp/www/tests/favicon/favicon.ico">
 	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+	<meta name="msapplication-TileImage" content="C:/wamp/www/tests/favicon/mstile-144x144.png">
+	<meta name="msapplication-config" content="C:/wamp/www/tests/favicon/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
 
 </head>
